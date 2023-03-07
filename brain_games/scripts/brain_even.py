c59e8a2957f5c3fe7c472c2d63a35bf6d7710f91
Ivan Granovsky <ivanator666@gmail.com>
2023-03-07 19:24:48 +0000

    cleaning

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -9,15 +9,15 @@ def is_even(num):
 
 def game():
     question = randint(0, 100)
-    answer, condition = input(f'Question: {question}\nYour answer: '), is_even(question)
+    answer = input(f'Question: {question}\nYour answer: ')
+    cond = is_even(question)
 
-    if answer == 'yes' and condition or answer == 'no' and not condition:
+    if answer == 'yes' and cond or answer == 'no' and not cond:
         print('Correct!')
     else:
-        correct_answer = 'yes' if is_even(question) else 'no'
-        print(f'"{answer}" is wrong answer ;(. Correct answer was "{correct_answer}".')
+        right_answer = 'yes' if is_even(question) else 'no'
+        print(f'"{answer}" is wrong answer ;(. Correct answer was "{right_answer}".')
         return ''
-
 
 
 def start_game(name):
@@ -32,7 +32,8 @@ def main():
     print('Welcome to the Brain Games!')
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    start_game(name)        
+    start_game(name)
+
 
 if __name__ == '__main__':
     main()
