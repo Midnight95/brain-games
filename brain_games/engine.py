1e4fb8ca756e6666f17c25e4715cc9c39f342c0c
midnight <ivanator666@gmail.com>
2023-03-12 22:17:13 +0300

    refactored engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -13,7 +13,8 @@ def start_game(module):
         if answer == correct_answer:
             print('Correct!')
         else:
-            print(f"'{answer}' is wrong answer ;(.Correct answer was '{correct_answer}'.")
+            print(f"'{answer}' is wrong answer ;(."
+                  f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
     else:
