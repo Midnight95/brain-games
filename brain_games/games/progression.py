a5755d873820160951aa92e453ca730845f71db3
midnight <ivanator666@gmail.com>
2023-04-17 01:07:57 +0300

    recactored progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,14 +5,19 @@ RANGE_START, RANGE_END = 5, 16
 STEP_START, STEP_END = 1, 11
 
 
+def make_progression(step, length):
+    return [str(step * i) for i in range(1, length + 1)]
+
+
 def build_question_answer_pair():
-    length = randint(RANGE_START, RANGE_END)
     step = randint(STEP_START, STEP_END)
-    progression = [str(step * i) for i in range(1, length + 1)]
+    length = randint(RANGE_START, RANGE_END)
+
+    progression = make_progression(step, length)
 
     correct_answer = (choice(progression))
 
     progression[progression.index(correct_answer)] = '..'
-    question = " ".join(progression)
+    question = ' '.join(progression)
 
     return question, correct_answer
